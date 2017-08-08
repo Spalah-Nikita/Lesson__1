a661e691638d7bf4ef0da54a5c1d04dab696abd9
Nikita Vintonovich <nikitavintonovich@MacBook-Pro-Nikita.local>
2017-08-08 21:13:00 +0300

    Первый коммит. Добавили проект

---
+++ Lesson_1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lesson_1
+//
+//  Created by Nikita Vintonovich on 8/8/17.
+//  Copyright © 2017 Vint-Rock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
